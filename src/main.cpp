7ab6536354a8a9dc29dc41c39f03362b0d6875f1
Валентина Макарова <valia7890123@gmail.com>
2023-11-08 13:18:48 +0300

    лабораторная 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 int main () {
-
+  rcc_periph_clock_enable(RCC_GPIOE);
+  gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
     while (true) {
+        gpio_toggle(GPIOE, GPIO9);
+        for(volatile uint32_t i = 0; i < 500'000; ++i);
 
     }
 }
