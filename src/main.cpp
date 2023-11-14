ed873eb82ec7cc374eebae9be8e4cca7b266e748
Валентина Макарова <valia7890123@gmail.com>
2023-11-14 12:25:56 +0300

    Разгон процессора

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 int main () {
+  rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
   rcc_periph_clock_enable(RCC_GPIOE);
   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
     while (true) {
